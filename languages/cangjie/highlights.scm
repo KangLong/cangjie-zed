8cd0cb674ac9c8736d6a51f6641b8d2152113cea
KangLong <bwilling@126.com>
2026-06-18 22:25:57 +0800

    Fix highlights.scm: remove from keyword, fix indent captures

--- languages/cangjie/highlights.scm
+++ languages/cangjie/highlights.scm
@@ -1,252 +1,126 @@
-; ============================================================
-; Cangjie (仓颉) Tree-sitter syntax highlights for Zed
-; ============================================================
+; Cangjie syntax highlighting for Zed
 
-; -----------------------------------------------------------
+; ============================================
 ; Comments
-; -----------------------------------------------------------
+; ============================================
 (lineComment) @comment
 (blockComment) @comment
 
-; -----------------------------------------------------------
+; ============================================
 ; Literals
-; -----------------------------------------------------------
+; ============================================
+(stringLiteral) @string
 (integerLiteral) @number
 (floatLiteral) @number.float
 (runeLiteral) @string.special
 (booleanLiteral) @boolean
 (unitLiteral) @constant.builtin
-(stringLiteral) @string
-(wildcardPattern) @constant.builtin       ; wildcard _
+(wildcardPattern) @constant.builtin
 
-; -----------------------------------------------------------
-; String interpolation
-; -----------------------------------------------------------
-(inlineExpression) @embedded
-(inMultiLineStringExpression) @embedded
+; ============================================
+; Variables & Parameters
+; ============================================
+(varBindingPattern) @variable
+(parameter
+  paraName: (identifier) @variable.parameter)
+(thisSuperExpression) @variable.builtin
 
-; -----------------------------------------------------------
-; Built-in types (primitive type keywords)
-; -----------------------------------------------------------
-[
-  "Int8" "Int16" "Int32" "Int64" "IntNative"
-  "UInt8" "UInt16" "UInt32" "UInt64" "UIntNative"
-  "Float16" "Float32" "Float64"
-  "Rune"
-  "Bool"
-  "Nothing"
-  "Unit"
-  "This"
-] @type.builtin
+; ============================================
+; Functions
+; ============================================
+(funcName) @function
+(callSuffix) @function.call
 
-(String) @type.builtin
-(VArray) @type.builtin
-
-; -----------------------------------------------------------
-; User-defined types (class/struct/enum/interface names)
-; -----------------------------------------------------------
+; ============================================
+; Types (user-defined)
+; ============================================
 (className) @type
 (structName) @type
 (enumName) @type
 (interfaceName) @type
 (typeAliasName) @type
-(extendsType) @type
-
-; -----------------------------------------------------------
-; Functions & methods
-; -----------------------------------------------------------
-(funcName) @function
-(functionDefinition
-  name: (funcName) @function) @function
-
-(operatorFunctionDefinition
-  operator: _ @operator) @function
-
-(callSuffix) @function.call
-
-; -----------------------------------------------------------
-; Properties & fields
-; -----------------------------------------------------------
 (propertyName) @property
-(propertyDefinition
-  name: (propertyName) @property) @property
-
-; -----------------------------------------------------------
-; Variables & parameters
-; -----------------------------------------------------------
-(varBindingPattern) @variable
-(parameter
-  paraName: (identifier) @variable.parameter)
-
-; -----------------------------------------------------------
-; this / super
-; -----------------------------------------------------------
-(thisSuperExpression) @variable.builtin
-
-; -----------------------------------------------------------
-; Return type annotations
-; -----------------------------------------------------------
 (returnType
   (_) @type)
 
-; -----------------------------------------------------------
-; Keywords — modifiers (visibility, static, abstract etc.)
-; -----------------------------------------------------------
+; ============================================
+; Modifiers → @keyword
+; ============================================
 (modifiers) @keyword
 
-; -----------------------------------------------------------
-; Keywords — declaration/control-flow keywords (as tokens)
-; -----------------------------------------------------------
+; ============================================
+; Keyword tokens (anonymous)
+; ============================================
 [
-  "package"
-  "import"
-  "class"
-  "struct"
-  "enum"
-  "interface"
-  "func"
-  "main"
-  "let"
-  "var"
-  "const"
-  "init"
-  "prop"
-  "operator"
-  "macro"
-  "quote"
-  "extend"
-  "type"
-  "foreign"
-  "inout"
-  "where"
-  "mut"
-  "unsafe"
-  "spawn"
-  "synchronized"
-  "open"
-  "override"
-  "redef"
-  "sealed"
-  "abstract"
-  "get"
-  "set"
+  "package" "import" "as"
+  "class" "struct" "enum" "interface"
+  "func" "main"
+  "let" "var" "const"
+  "init" "prop"
+  "operator" "macro" "quote" "extend"
+  "type" "foreign" "inout" "where"
+  "mut" "unsafe" "spawn" "synchronized"
+  "open" "override" "redef" "sealed" "abstract"
+  "public" "private" "protected" "static" "internal"
+  "get" "set"
 ] @keyword
 
-; -----------------------------------------------------------
-; Keywords — control flow
-; -----------------------------------------------------------
+; ============================================
+; Control flow keywords
+; ============================================
 [
-  "if"
-  "else"
-  "for"
-  "in"
-  "do"
-  "while"
-  "match"
-  "case"
-  "try"
-  "catch"
-  "finally"
-  "throw"
-  "return"
-  "break"
-  "continue"
-  "is"
-  "as"
-  "super"
-  "this"
+  "if" "else"
+  "for" "in" "do" "while"
+  "match" "case"
+  "try" "catch" "finally" "throw"
+  "return" "break" "continue"
+  "is" "as"
+  "super" "this"
 ] @keyword
 
-; -----------------------------------------------------------
-; Boolean keywords
-; -----------------------------------------------------------
-[
-  "true"
-  "false"
-] @boolean
+; ============================================
+; Boolean literals
+; ============================================
+["true" "false"] @boolean
 
-; -----------------------------------------------------------
+; ============================================
+; Built-in types (named nodes from grammar aliases)
+; ============================================
+[
+  (String)
+  (Int8) (Int16) (Int32) (Int64) (IntNative)
+  (UInt8) (UInt16) (UInt32) (UInt64) (UIntNative)
+  (Float16) (Float32) (Float64)
+  (Rune) (Bool) (Nothing) (Unit) (Thistype)
+] @type.builtin
+
+; ============================================
 ; Operators
-; -----------------------------------------------------------
+; ============================================
 [
-  "."
-  ","
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "**"
-  "*"
-  "%"
-  "/"
-  "+"
-  "-"
-  "++"
-  "--"
-  "&&"
-  "||"
-  "!"
-  "&"
-  "|"
-  "^"
-  "~"
-  "<<"
-  ">>"
-  ":"
-  ";"
-  "="
-  "+="
-  "-="
-  "*="
-  "**="
-  "/="
-  "%="
-  "&="
-  "|="
-  "^="
-  "<<="
-  ">>="
-  "&&="
-  "||="
-  "->"
-  "<-"
-  "=>"
-  ".."
-  "..="
-  "..."
+  "+" "-" "*" "/" "%" "**"
+  "++" "--"
+  "==" "!=" "<" ">" "<=" ">="
+  "!" "~"
+  "&&" "||"
+  "&" "|" "^" "<<" ">>"
+  "=" "+=" "-=" "*=" "/=" "%="
+  "**=" "&=" "|=" "^=" "<<=" ">>="
+  "&&=" "||="
+  "?" "??" "|>" "~>"
+  ".." "..=" "..."
+  "->" "<-" "=>" "<:"
   "@"
-  "?"
-  "<:"
-  "<"
-  ">"
-  "<="
-  ">="
-  "!="
-  "=="
-  "|>"
-  "~>"
-  "??"
 ] @operator
 
-; -----------------------------------------------------------
-; Punctuation delimiters
-; -----------------------------------------------------------
-[
-  ";"
-  ","
-  "."
-] @punctuation.delimiter
+; ============================================
+; Punctuation
+; ============================================
+[ ";" "," "." ] @punctuation.delimiter
+[ "(" ")" "{" "}" "[" "]" ] @punctuation.bracket
 
-; -----------------------------------------------------------
-; Brackets
-; -----------------------------------------------------------
-[
-  "("
-  ")"
-  "{"
-  "}"
-  "["
-  "]"
-] @punctuation.bracket
+; ============================================
+; String interpolation
+; ============================================
+(inlineExpression) @embedded
+(inMultiLineStringExpression) @embedded
