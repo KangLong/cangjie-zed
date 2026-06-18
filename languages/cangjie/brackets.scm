9792a41f0c1ec48db07b046568c56750398f924f
KangLong <bwilling@126.com>
2026-06-18 22:24:07 +0800

    Fix highlights.scm: remove from keyword, fix indent captures

--- languages/cangjie/brackets.scm
+++ languages/cangjie/brackets.scm
@@ -1,6 +1,3 @@
-; Cangjie bracket matching for Zed
-; Opening and closing bracket pairs
-
 ("(" @open ")" @close)
 ("{" @open "}" @close)
 ("[" @open "]" @close)
--- languages/cangjie/outline.scm
+++ languages/cangjie/outline.scm
@@ -1,43 +1,5 @@
-; Cangjie outline rules for Zed
-
-; Package declaration
-(packageDeclaration
-  packageName: (_) @name) @item
-
-; Functions
-(functionDefinition
-  (funcName) @name) @item
-
-; Operator functions
-(operatorFunctionDefinition
-  operator: (_) @name) @item
-
-; Class definitions
-(classDefinition
-  (className) @name) @item
-
-; Struct definitions
-(structDefinition
-  (structName) @name) @item
-
-; Enum definitions
-(enumDefinition
-  (enumName) @name) @item
-
-; Interface definitions
-(interfaceDefinition
-  (interfaceName) @name) @item
-
-; Properties
-(propertyDefinition
-  (propertyName) @name) @item
-
-; Type aliases
-(typeAlias
-  (typeAliasName) @name) @item
-
-; Init constructor
-(init) @item
-
-; Variable declarations at top level
-(variableDeclaration) @item
+(functionDefinition (funcName) @name) @item
+(classDefinition (className) @name) @item
+(structDefinition (structName) @name) @item
+(enumDefinition (enumName) @name) @item
+(interfaceDefinition (interfaceName) @name) @item
